071db1d65edbdad3bd44ea9bfa97822993202da3
husiatin <73538365+husiatin@users.noreply.github.com>
2024-12-02 18:43:31 +0100

    Create writeyaml.h

---
+++ writeyaml.h
@@ -0,0 +1,9 @@
+#ifndef LOGGING_H
+#define LOGGING_H
+
+// Deklaration der Funktion
+void writeResultsToYaml(float avgKeyPresses, float avgKeyPressesInterval, float avgEnterPresses,
+                        float avgBackspacePresses, float avgClicks, float avgLeftClicks,
+                        float avgRightClicks, float clickDifference);
+
+#endif
