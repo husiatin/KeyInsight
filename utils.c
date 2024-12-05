98b7a7577eff87fe13961d8a8815be4a82830cf1
Ryu <151095211+MindDaemon@users.noreply.github.com>
2024-12-06 00:37:25 +0100

    Add files via upload

---
+++ utils.c
@@ -0,0 +1,15 @@
+#include "utils.h"
+
+float calculateAverage(float *data, int size)
+{
+    float sum = 0.0f;
+
+    for (int i = 0; i < size; i++)
+    {
+        if (data[i] > 0)
+        {
+            sum += data[i];
+        }
+    }
+    return (size == 0) ? 0.0f : sum / (float)size;
+}
